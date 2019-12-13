347690b5629a54d6ead07c8b05838f061edc8986
jason <jiang911113@163.com>
2019-12-13 13:37:01 +0800

    fix

--- src/main/resources/static/blog/admin/js/article.js
+++ src/main/resources/static/blog/admin/js/article.js
@@ -62,12 +62,13 @@ function subArticle(status) {
     });
 }
 
+/*
 var textarea = $('#text'),
     toolbar = $('<div class="markdown-editor" id="md-button-bar" />').insertBefore(textarea.parent())
 preview = $('<div id="md-preview" class="md-hidetab" />').insertAfter('.markdown-editor');
 
 markdown(textarea, toolbar, preview);
-
+*/
 
 function allow_comment(obj) {
     var this_ = $(obj);
