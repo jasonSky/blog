7b63a34e5b26f23a9ba9e3265f60efce0b0f45ae
jason <jiang911113@163.com>
2020-01-16 17:26:10 +0800

    fix

--- php/printLog.php
+++
@@ -1,4 +0,0 @@
-    <?php
-    @header('Content-type: text/plain;charset=UTF-8');
-    $filepath = './log/log.xml';
-    readfile($filepath);
--- php/printQuery.php
+++
@@ -1,4 +0,0 @@
-<?php
-@header('Content-type: text/plain;charset=UTF-8');
-$filepath = './log/query.xml';
-readfile($filepath);
